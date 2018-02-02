fcfbfbe2eec60f0b346d72c9370420d0d4da452b
lucas suarez <lucas.suarez.utepsa@gmail.com>
2018-02-02 17:57:46 -0400

    Proyecto configurado y listo para usarse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+// Se declaran las variables que jquery va usar
+declare var jquery:any;
+declare var $ :any;
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+
+
+  constructor(){
+
+  }
+
+  menuToggle(){
+
+    $(".button-collapse").sideNav();
+
+  }
 }
